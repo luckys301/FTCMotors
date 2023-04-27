a3f7cab88c1d591eba5811bd8d1231d084b252bb
FTC10862Nebula <droidrage.nebula@gmail.com>
2023-04-26 22:54:31 -0500

    RoadrunnerValues (To Test)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/roadrunner/RoadrunnerValues.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.opmode.roadrunner;
+
+
+public class RoadrunnerValues {
+    public static StrafeTest strafeTest;
+    public static class StrafeTest {
+        public static double DISTANCE = 60;
+    }
+}
