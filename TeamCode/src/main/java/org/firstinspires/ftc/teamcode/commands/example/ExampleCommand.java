13f886317bb7da913be22829784674760e6043da
FTC10862Nebula <droidrage.nebula@gmail.com>
2023-04-28 11:44:40 -0500

    Instant Command Writing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/example/ExampleCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/example/ExampleCommand.java
@@ -11,8 +11,11 @@ import org.firstinspires.ftc.teamcode.subsystems.mecDrive.MecDrivetrainSubsystem
 //nowadays lance isnt a very common name, but in older times people were named lance a lot
 //List of Commands: https://docs.ftclib.org/ftclib/v/v2.0.0/command-base/command-system/convenience-commands
 public class ExampleCommand extends SequentialCommandGroup{
-    public ExampleCommand(){
+    public ExampleCommand(Pivot pivot){
         addRequirements();    //Add Subsystems that you need to run this Command - not necessary
-        addCommands();
+        addCommands(
+//                new InstantCommand(claw::setFClawPos)
+//                new InstantCommand(()->{pivot.moveIntakeBAuto();}) // This is how you use lamdas - Useful for commands that have parameters
+        );
     }
 }
