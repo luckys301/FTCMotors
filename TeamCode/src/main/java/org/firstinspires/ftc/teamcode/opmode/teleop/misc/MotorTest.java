4e636db977f6fe6ef45185bc0ef3754b5c75ffb3
FTC10862Nebula <droidrage.nebula@gmail.com>
2023-04-28 12:09:09 -0500

    Created Servo/Motor Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/misc/MotorTest.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop.misc;
+
+
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.util.nebulaHardware.NebulaMotor;
+
+//@Disabled
+@TeleOp
+public class MotorTest extends OpMode {
+    NebulaMotor motor;
+    @Override
+    public void init() {
+        motor = hardwareMap.get(NebulaMotor.class, "motor");
+        motor = new NebulaMotor(hardwareMap,
+                "motorName",
+                Motor.GoBILDA.RPM_84,
+                NebulaMotor.Direction.Forward,
+                Motor.ZeroPowerBehavior.FLOAT,
+                true);
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.dpad_down){motor.setPower(1);}
+        else if(gamepad1.dpad_up){motor.setPower(-1);}
+        else{motor.setPower(0);}
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/misc/ServoTest.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop.misc;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//@Disabled
+@TeleOp
+public class ServoTest extends OpMode {
+    com.qualcomm.robotcore.hardware.Servo servo;
+    double pos = 0;
+    @Override
+    public void init() {
+        servo = hardwareMap.get(com.qualcomm.robotcore.hardware.Servo.class, "clawS2");
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.dpad_down){pos -= 0.001;}
+        else if(gamepad1.dpad_up){pos += 0.001;}
+
+        if(gamepad1.right_bumper){pos = 0;}
+        else if(gamepad1.left_bumper){pos = 1;}
+
+        pos = Math.min(Math.max(pos, 0), 1);
+        servo.setPosition(pos);
+        telemetry.addData("ServoTest Pos: ", servo.getPosition());
+        telemetry.addData("Desired Pos: ", pos);
+        telemetry.update();
+
+    }
+}
