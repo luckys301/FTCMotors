40d372386a153482306c82ed6c85451560c87db3
FTC10862Nebula <droidrage.nebula@gmail.com>
2023-04-30 01:35:13 -0500

    Added Constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/NebulaConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/NebulaConstants.java
@@ -19,7 +19,7 @@ public class NebulaConstants {
     public static class Pivot {
         public static String pivotMName = "clawM";
         public NebulaMotor.Direction pivotDirection = NebulaMotor.Direction.Reverse;
-//        public int pivotDistancePerPulse = 360 / (gear_ratio * pulses_per_rev);
+//        public int pivotDistancePerPulse = 360 / (gear_ratio * pulses_per_rev);// For Internal Encoder
 
         public static PIDFCoefficients pivotPID = new PIDFCoefficients(.005, 0.00, 0.0,0);
         public static int pivotTolerance = 10;
@@ -28,7 +28,9 @@ public class NebulaConstants {
             ka=0,
             kv=0;
         public static double maxVelocity = 0,
-            maxAcceleration = 0;
+            maxAcceleration = 0,
+            MIN_POSITION = Math.toRadians(20),
+            MAX_POSITION = Math.toRadians(230);
     }
     public Claw claw;
     public static class Claw {
@@ -44,5 +46,27 @@ public class NebulaConstants {
         public static PIDFCoefficients slidePID = new PIDFCoefficients(0.005, 0.00, 0,0);//.0075, 0., .003, 0)
         public static int slideTolerance = 10;
 //        public int slideDistancePerPulse = (COUNTS_PER_PULSE * GEAR_RATIO) / (GEAR_DIAMETER_INCHES * Math.PI);
+        public static double ks=0,
+            kcos=0,
+            ka=0,
+            kv=0;
+        public static double maxVelocity = 0,
+            maxAcceleration = 0,
+            MIN_POSITION = 0,//mm
+            MAX_POSITION = 0;
+    }
+    public Drive drive;
+    public static class Drive {//TODO:FIX CONSTANTS
+        public static String leftFrontM = "leftFront";
+        public static String leftRearM = "leftRear";
+        public static String rightRearM = "rightRear";
+        public static String rightFrontM = "rightFront";
+        public NebulaMotor.Direction leftFrontDir = NebulaMotor.Direction.Reverse,
+            leftRearDir = NebulaMotor.Direction.Reverse,
+            rightRearDir = NebulaMotor.Direction.Forward,
+            rightFrontDir = NebulaMotor.Direction.Forward;
+        public static boolean isSquaredInputs = true;
+        public static double tippingTolerance = 5;
+
     }
 }
