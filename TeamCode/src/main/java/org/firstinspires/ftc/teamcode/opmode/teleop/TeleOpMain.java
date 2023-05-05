5d8f79755841a4cee57b8891d73949ad038816a3
FTC10862Nebula <droidrage.nebula@gmail.com>
2023-05-04 19:10:01 -0500

    Figured out .and and .or Buttons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleOpMain.java
@@ -1,15 +1,19 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.commands.drive.teleop.DefaultDriveCommand;
+import org.firstinspires.ftc.teamcode.commands.drive.teleop.mec.DefaultMecDriveCommand;
 import org.firstinspires.ftc.teamcode.subsystems.Claw;
 import org.firstinspires.ftc.teamcode.subsystems.drive.mecDrive.MecDriveSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.pivot.Pivot;
 import org.firstinspires.ftc.teamcode.subsystems.slide.Slide;
 import org.firstinspires.ftc.teamcode.subsystems.drive.mecDrive.MecDrive;
+import org.firstinspires.ftc.teamcode.util.teleop.GamepadTrigger;
 import org.firstinspires.ftc.teamcode.util.teleop.MatchOpMode;
 
 @Config
@@ -46,13 +50,23 @@ public class TeleOpMain extends MatchOpMode {
         driverGamepad.gamepad.rumble(1,1, 100);
 
         //Ways to use buttons
-//        new GamepadButton(driverGamepad, GamepadKeys.Button.LEFT_BUMPER).and()
-//            .whileHeld(new SlowDriveCommand(mecDriveSubsystem, driverGamepad, true));
+        new GamepadButton(driverGamepad, GamepadKeys.Button.LEFT_BUMPER)
+            .and(new GamepadTrigger(operatorGamepad, GamepadKeys.Trigger.LEFT_TRIGGER)).negate()
+            .whileActiveContinuous(new SequentialCommandGroup());
+        new GamepadButton(driverGamepad, GamepadKeys.Button.LEFT_BUMPER)
+            .and(new GamepadTrigger(operatorGamepad, GamepadKeys.Trigger.LEFT_TRIGGER))
+            .whileActiveContinuous(new SequentialCommandGroup());
+//        new GamepadTrigger(driverGamepad, GamepadKeys.Trigger.LEFT_TRIGGER)
+//            .and(new GamepadButton(operatorGamepad, GamepadKeys.Button.LEFT_BUMPER))
+//            .whileActiveContinuous(new SlowMecDriveCommand(mecDriveSubsystem, driverGamepad, true));
+//        new GamepadButton(driverGamepad, GamepadKeys.Button.LEFT_BUMPER)
+//            .and(new GamepadTrigger(operatorGamepad, GamepadKeys.Trigger.LEFT_TRIGGER))
+//            .whileActiveContinuous(new SlowMecDriveCommand(mecDriveSubsystem, driverGamepad, true))
 //        new GamepadButton(driverGamepad, GamepadKeys.Button.LEFT_BUMPER).or()
-//            .whileHeld(new SlowDriveCommand(mecDriveSubsystem, driverGamepad, true));
+//            .whileHeld(new SlowMecDriveCommand(mecDriveSubsystem, driverGamepad, true));
 //        new GamepadButton(driverGamepad, GamepadKeys.Button.LEFT_BUMPER).and().negate()
-//            .whileHeld(new SlowDriveCommand(mecDriveSubsystem, driverGamepad, true));
-        mecDriveSubsystem.setDefaultCommand(new DefaultDriveCommand(mecDriveSubsystem, driverGamepad, true));
+//            .whileHeld(new SlowMecDriveCommand(mecDriveSubsystem, driverGamepad, true));
+        mecDriveSubsystem.setDefaultCommand(new DefaultMecDriveCommand(mecDriveSubsystem, driverGamepad, true));
     }
 
     @Override
