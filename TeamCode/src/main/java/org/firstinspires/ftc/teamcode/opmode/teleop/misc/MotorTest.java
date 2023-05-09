d5e55e939d7951338a5b1d16f6c6e6b45799f94c
FTC10862Nebula <droidrage.nebula@gmail.com>
2023-05-08 23:58:56 -0500

    Nebula Motor Works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/misc/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/misc/MotorTest.java
@@ -2,10 +2,8 @@ package org.firstinspires.ftc.teamcode.opmode.teleop.misc;
 
 
 import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.util.nebulaHardware.NebulaMotor;
 
@@ -15,13 +13,13 @@ public class MotorTest extends OpMode {
     NebulaMotor motor;
     @Override
     public void init() {
-        motor = hardwareMap.get(NebulaMotor.class, "motor");
+//        motor = hardwareMap.get(NebulaMotor.class, "leftRear");
         motor = new NebulaMotor(hardwareMap,
-                "motorName",
-                Motor.GoBILDA.RPM_84,
+                "leftRear",
+                Motor.GoBILDA.RPM_435,
                 NebulaMotor.Direction.Forward,
                 NebulaMotor.IdleMode.Coast,
-                true);
+                false);
     }
 
     @Override
