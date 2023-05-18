f787c8da9b3273a009a5dc477eb4d91015ad660a
FTC10862Nebula <droidrage.nebula@gmail.com>
2023-05-17 23:44:54 -0500

    Added Motor Type (TODO: Change to Motor Type)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/nebulaHardware/NebulaMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/nebulaHardware/NebulaMotor.java
@@ -5,6 +5,27 @@ import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class NebulaMotor{
+    public enum MotorType {
+        RPM_30(5264, 30), RPM_43(3892, 43), RPM_60(2786, 60), RPM_84(1993.6, 84),
+        RPM_117(1425.2, 117), RPM_223(753.2, 223), RPM_312(537.6, 312), RPM_435(383.6, 435),
+        RPM_1150(145.6, 1150), RPM_1620(103.6, 1620), BARE(28, 6000), NONE(0, 0);
+
+        private double cpr, rpm;
+
+        MotorType(double cpr, double rpm) {
+            this.cpr = cpr;
+            this.rpm = rpm;
+        }
+        public double getCPR() {
+            return cpr;
+        }
+        public double getRPM() {
+            return rpm;
+        }
+        public double getAchievableMaxTicksPerSecond() {
+            return cpr * rpm / 60;
+        }
+    }
     public enum IdleMode {
         Coast,
         Brake
@@ -24,7 +45,9 @@ public class NebulaMotor{
 //    private final Motor.GoBILDA type;
 //    private int gearing;
 
-    public NebulaMotor(HardwareMap hM, String deviceId, Motor.GoBILDA type, Direction direction, IdleMode behavior, Boolean isEnabled){
+    public NebulaMotor(HardwareMap hM, String deviceId,
+                       Motor.GoBILDA type, Direction direction,
+                       IdleMode behavior, Boolean isEnabled){
         motor = new MotorEx(hM, deviceId, type);
 //        this.type = type;
         this.isEnabled = isEnabled;
